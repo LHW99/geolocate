1c501dcb0ef1a78fb3618b28df036808d4809763
haiduk <hwlee91@gmail.com>
2021-03-31 00:43:19 -0400

    trying pypi googlemaps

--- locate/views.py
+++ locate/views.py
@@ -4,11 +4,21 @@ try:
 except:
   print('ok')
 import requests
+import googlemaps
+from datetime import datetime
+
+#    response = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key={API_KEY}&callback=initMap')
+#    data = response.json()
+#    print(data)
+
+gmaps = googlemaps.Client(key=API_KEY)
 
 def home(request):
   if request.method == 'GET':
-    response = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key={API_KEY}&callback=initMap')
-    data = response.json()
-    print(data)
 
-  return render(request, 'home.html')
+    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+
+  return render(request, 'home.html', {
+    'result': geocode_result,
+  })
